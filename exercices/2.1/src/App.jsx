93dd1ce359d52a67c9db4fff2dd8719b85808f21
khelifa yassin <yassin.khelifa@student.vinci.be>
2024-10-29 17:42:53 +0100

    new:ex2.1-destructuring

--- exercices/2.1/src/App.jsx
+++ exercices/2.1/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <ClickCounter />
+      <ClickCounter title="Click Counter" message="You are a master in the art of clicking !" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- exercices/2.1/src/components/ClickCounter/ClickCounter.jsx
+++ exercices/2.1/src/components/ClickCounter/ClickCounter.jsx
@@ -1,16 +1,19 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
-const ClickCounter = () => {
+const ClickCounter = ({title, message}) => {
     const [count, setCount] = useState(0);
 
     return (
         <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+            <h2>{title}</h2>
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <p>{count >= 10 ? message : ""}</p>
+            <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
       </div>
       )};
 
