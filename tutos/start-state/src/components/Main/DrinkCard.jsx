269f2f5e2d1809c6346bfd315d9e8e257b322fa3
khelifa yassin <yassin.khelifa@student.vinci.be>
2024-10-29 17:37:18 +0100

    new:ex2.1-init

---
+++ tutos/start-state/src/components/Main/DrinkCard.jsx
@@ -0,0 +1,12 @@
+const DrinkCard = ({ title, image, children }) => {
+  return (
+    <div className="drink-card">
+      <img src={image} alt={title} className="drink-image" width="50" />
+      <h2>{title}</h2>
+      <div className="drink-details">{children}</div>
+    </div>
+  );
+};
+
+
+export default DrinkCard;
---
+++ tutos/start-state/src/components/Main/DrinkMenu.jsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+
+const DrinkMenu = ({ title, children }) => {
+  return (
+    <div className="drink-menu">
+      <h4>{title}</h4>
+      <div className="drink-items">{children}</div>
+    </div>
+  );
+};
+
+
+export default DrinkMenu;
