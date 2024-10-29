b0a7d0dac47132e7be2ba67b3d07305efe289cb3
khelifa yassin <yassin.khelifa@student.vinci.be>
2024-10-29 18:08:50 +0100

    new:ex2.1-stored

--- exercices/2.1/src/components/ClickCounter/ClickCounter.jsx
+++ exercices/2.1/src/components/ClickCounter/ClickCounter.jsx
@@ -2,22 +2,29 @@
 import { useState } from "react";
 
 const ClickCounter = ({title, message, mouseHover}) => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(JSON.parse(localStorage.getItem("count")) !== null ? JSON.parse(localStorage.getItem("count")) : 0);
     const [isHovered, setIsHovered] = useState(false);
 
     return (
         <div className="card">
             <h2>{title}</h2>
             <p>{isHovered ? mouseHover : ""}</p>
-            <button onClick={() => setCount((count) => count + 1)} 
+            <button onClick={() => {
+                setCount((count) => {
+                    const newCount = count + 1;
+                    localStorage.setItem("count", JSON.stringify(newCount));
+                    return newCount;
+                });
+            }} 
             onMouseEnter={() => setIsHovered(!isHovered)} onMouseLeave={() => setIsHovered(!isHovered)}>
                 count is {count}
             </button>
             <p>{count >= 10 ? message : ""}</p>
-            <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-      </div>
-      )};
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+        );
+    };
 
 export default ClickCounter;
